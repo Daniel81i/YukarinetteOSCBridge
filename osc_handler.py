f661db88e0d16ca67e2ffa8dcc014acd3caf1358
Daniel81i <166271277+Daniel81i@users.noreply.github.com>
2025-12-28 18:00:30 +0900

    Update osc_handler.py

--- osc_handler.py
+++ osc_handler.py
@@ -32,7 +32,6 @@ class OSCHandler:
     # ---------------------------
     async def _handle_input(self, address, *args):
         value = args[0]
-        logging.debug(f"OSC Recv Input: {value}")
         if logging.getLogger().isEnabledFor(logging.DEBUG):
             logging.debug(f"[OSC HANDLER] addr={address}, args={args}, value={value}")
         await self.on_input(value)
